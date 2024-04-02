5aa5e53c8721aae1bb61ad61d08659887c32e866
hasan7n <hasankassim7@hotmail.com>
2024-04-02 04:43:25 +0200

    debug

--- test.sh
+++ test.sh
@@ -15,8 +15,13 @@ doo </dev/null &>agg.log &
 q=$!
 echo $q
 
-[ -d "/proc/${p}" ] && echo "p exists" || echo "p not exists"
-[ -d "/proc/${q}" ] && echo "q exists" || echo "q not exists"
+echo "/proc/$p"
+echo "/proc/$q"
+echo "/proc/${p}"
+echo "/proc/${q}"
+
+[ -d "/proc/$p" ] && echo "p exists" || echo "p not exists"
+[ -d "/proc/$q" ] && echo "q exists" || echo "q not exists"
 
 echo "waiting p"
 wait $p
