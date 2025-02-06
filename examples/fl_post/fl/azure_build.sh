ebe27208430bd72cc76b012b2f78e5095d64361a
Brandon Edwards <brandon.edwards@intel.com>
2025-02-06 01:01:32 +0000

    now put hasan_openfl into fl_post/fl folder and run azure_build.sh after making changes (plan to use temp_debug) branch

--- examples/fl_post/fl/azure_build.sh
+++ examples/fl_post/fl/azure_build.sh
@@ -1,20 +1,3 @@
-while getopts b flag; do
-    case "${flag}" in
-    b) BUILD_BASE="true" ;;
-    esac
-done
-BUILD_BASE="${BUILD_BASE:-false}"
+cp project/debug_Dockerfile project/Dockerfile
 
-# copy over changes from be_Dockerfile to Dockerfile
-cp ./project/be_Dockerfile ./project/Dockerfile
-
-if ${BUILD_BASE}; then
-    git clone https://github.com/hasan7n/openfl.git
-    cd openfl
-    git checkout develop
-    docker build \
-	    -t local/openfl:local -f openfl-docker/Dockerfile.base .
-    cd ..
-    rm -rf openfl
-fi
-mlcube configure --mlcube ./mlcube -Pdocker.build_strategy=always -Pdocker.build_args="--build-arg http_proxy='http://proxy-us.intel.com:912' --build-arg https_proxy='http://proxy-us.intel.com:912'"
+mlcube configure --mlcube ./mlcube -Pdocker.build_strategy=always
