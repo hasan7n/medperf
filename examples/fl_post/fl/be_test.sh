861af44c85df84382fcda0f99675a1ee692a90a1
Edwards <edwardsb@spr-gpu02.jf.intel.com>
2025-02-12 10:30:44 -0800

    putting some echos into bash script to indicate when cols are launching etc.

--- examples/fl_post/fl/be_test.sh
+++ examples/fl_post/fl/be_test.sh
@@ -24,6 +24,7 @@ mv ./mlcube_agg/workspace/plan/plan.yaml ./mlcube_agg/workspace
 rm -r ./mlcube_agg/workspace/plan
 cp ./mlcube_agg/workspace/plan.yaml ./for_admin
 
+echo "...copying over the plan into mlcube_col# directories"
 
 for ((i=0; i< $NUM_COLS; i++))
     do
@@ -33,12 +34,14 @@ for ((i=0; i< $NUM_COLS; i++))
 
 # run aggregator if appropriate
 if [ "$RUN_AGGREGATOR" == "true" ]; then
+   echo "...Running aggregator"
    medperf --platform $PLATFORM mlcube run --mlcube ./mlcube_agg --task start_aggregator -P $AGG_PORT > agg.log & 
 fi
 
 # run collaborators
 for ((i=0; i< $NUM_COLS; i++))
     do  
+	echo "...lauching collaborator ${i}"
         data_path=COL_${i}_DATA_PATH
 	labels_path=COL_${i}_LABELS_PATH
 	medperf --platform $PLATFORM mlcube run --mlcube ./mlcube_col${i} --task train -e MEDPERF_PARTICIPANT_LABEL=col${i}@example.com --params data_path=$PWD/mlcube_col${i}/workspace/data,labels_path=$PWD/mlcube_col${i}/workspace/labels > col${i}.log & 
