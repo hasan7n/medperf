e54a4aab89a7f35a4dd65cd18c88cf63219e2edc
Edwards <edwardsb@spr-gpu02.jf.intel.com>
2025-02-25 17:50:18 -0800

    adding a script to grep across all collaborator logs

---
+++ examples/fl_post/fl/grep_logs_across_cols.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+STRING=$1
+AFTER=$2
+BEFORE=$3
+
+NUM_COLS=50
+
+if [ "$STRING" == "" ]; then
+	echo "First argument, STRING, is required and was not provided"
+	exit
+fi
+
+if [ "$AFTER" == "" ]; then
+	    AFTER=0
+fi
+
+if [ "$BEFORE" == "" ]; then
+	            BEFORE=0
+fi
+
+
+for ((i=0; i< $NUM_COLS; i++))
+    do
+        cat col${i}.log | grep -A $AFTER -B $BEFORE "$STRING"
+    done
+
